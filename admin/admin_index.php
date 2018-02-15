ced22e052635413d89eabd16a50d1ed235680e40
Steven <s_cadick2@fanshaweonline.ca>
2018-02-15 02:02:08 -0500

    initial commit with timed greetings

---
+++ admin/admin_index.php
@@ -0,0 +1,32 @@
+<?php
+	//ini_set('display_errors', 1);
+	//error_reporting(E_ALL);
+	require_once('phpscripts/config.php');
+	confirm_logged_in();
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>CMS Portal Login</title>
+</head>
+<body>
+	<h1>Welcome Company Name to your admin page</h1>
+	<?php
+		// Set Default timezone to Toronto's time zone
+		date_default_timezone_set('America/Toronto');
+
+		// 24-hour format of an hour without leading zeros (0 through 23)
+		$Hour = date('G');
+
+		if ( $Hour >= 5 && $Hour <= 11 ) { //between 5am and 11am good morning
+				echo "Good Morning";
+		} else if ( $Hour >= 12 && $Hour <= 18 ) { //between 12pm and 6pm good afternoon
+				echo "Good Afternoon";
+		} else if ( $Hour >= 19 || $Hour <= 4 ) { //between 7pm and 4am good evening
+				echo "Good Evening";
+		}
+		echo "<h2>Last login was on {$_SESSION['user_date']}</h2>";
+		?>
+</body>
+</html>
